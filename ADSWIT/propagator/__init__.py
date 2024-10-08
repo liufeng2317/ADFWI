0fa5acab69816e26a8241bd1edf44756108c1cf2
liufeng2317 <2397664955@qq.com>
2024-10-08 19:31:05 +0800

    UPDATE:change ADSWIT TO ADFWI

--- ADSWIT/propagator/__init__.py
+++
@@ -1,5 +0,0 @@
-from .elastic_propagator import ElasticPropagator
-
-from .acoustic_propagator import AcousticPropagator
-
-from .gradient_process import GradProcessor
--- ADSWIT/survey/__init__.py
+++
@@ -1,7 +0,0 @@
-from .source import Source
-
-from .receiver import Receiver
-
-from .survey import Survey
-
-from .data import SeismicData
